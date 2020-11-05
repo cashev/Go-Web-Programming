8b3058cf656bc7a2d415970596bca793bdac3e03
cashew <42924021+cashev@users.noreply.github.com>
2020-11-05 22:05:13 +0900

    生成するXMLにヘッダーを追加

--- ch07/03-xml_create_marshal/xml.go
+++ ch07/03-xml_create_marshal/xml.go
@@ -8,22 +8,22 @@ import (
 
 type Post struct {
 	XMLName xml.Name `xml:"post"`
-	Id string `xml:"id,attr"`
-	Content string `xml:"content"`
-	Author Author `xml:"author"`
+	Id      string   `xml:"id,attr"`
+	Content string   `xml:"content"`
+	Author  Author   `xml:"author"`
 }
 
 type Author struct {
-	Id string `xml:"id,attr"`
+	Id   string `xml:"id,attr"`
 	Name string `xml:",chardata"`
 }
 
 func main() {
 	post := Post{
-		Id: "1",
+		Id:      "1",
 		Content: "Hello World!",
 		Author: Author{
-			Id: "2",
+			Id:   "2",
 			Name: "Sau Sheong",
 		},
 	}
@@ -33,7 +33,7 @@ func main() {
 		fmt.Println("Error marshalling to XML:", err)
 		return
 	}
-	err = ioutil.WriteFile("post.xml", output, 0644)
+	err = ioutil.WriteFile("post.xml", []byte(xml.Header+string(output)), 0644)
 	if err != nil {
 		fmt.Println("Error writing XML to file:", err)
 		return
