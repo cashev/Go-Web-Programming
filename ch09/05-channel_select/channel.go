bf99f414d085e4a95bfaa56b6b94cd70fd9c6928
cashew <42924021+cashev@users.noreply.github.com>
2020-11-15 19:19:12 +0900

    channelのclose

--- ch09/05-channel_select/channel.go
+++ ch09/05-channel_select/channel.go
@@ -2,30 +2,34 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func callerA(c chan string) {
 	c <- "Hello World!"
+	close(c)
 }
 
 func callerB(c chan string) {
 	c <- "Hola Mundo!"
+	close(c)
 }
 
 func main() {
 	a, b := make(chan string), make(chan string)
 	go callerA(a)
 	go callerB(b)
-	for i := 0; i < 5; i++ {
-		time.Sleep(1 * time.Microsecond)
+	var msg string
+	ok1, ok2 := true, true
+	for ok1 || ok2 {
 		select {
-		case msg := <-a:
-			fmt.Printf("%s from A\n", msg)
-		case msg := <-b:
-			fmt.Printf("%s from B\n", msg)
-		default:
-			fmt.Println("Default")
+		case msg, ok1 = <-a:
+			if ok1 {
+				fmt.Printf("%s from A\n", msg)
+			}
+		case msg, ok2 = <-b:
+			if ok2 {
+				fmt.Printf("%s from B\n", msg)
+			}
 		}
 	}
 }
