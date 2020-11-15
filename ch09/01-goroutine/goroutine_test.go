e70e80088f3db36abd3a8fb17ec9e53baac88045
cashew <42924021+cashev@users.noreply.github.com>
2020-11-15 17:54:55 +0900

    関数をGoroutineにした場合のベンチマーク

--- ch09/01-goroutine/goroutine_test.go
+++ ch09/01-goroutine/goroutine_test.go
@@ -30,3 +30,15 @@ func BenchmarkGoPrint1(b *testing.B) {
 		goPrint1()
 	}
 }
+
+func BenchmarkPrint2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		print2()
+	}
+}
+
+func BenchmarkGoPrint2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		goPrint2()
+	}
+}
